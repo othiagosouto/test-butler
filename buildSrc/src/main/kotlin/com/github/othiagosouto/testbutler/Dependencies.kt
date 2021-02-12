007a2d98a72ed054316195b3992724d1fafc15dc
Thiago Souto <soutosss@gmail.com>
2021-02-12 19:25:10 -0300

    refactor dependencies and build gradle

--- buildSrc/src/main/kotlin/com/github/othiagosouto/testbutler/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/github/othiagosouto/testbutler/Dependencies.kt
@@ -5,11 +5,19 @@ class Test {
 }
 
 class Core {
-    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.10"
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.20"
+}
+
+class Utils {
+    val dokka = "org.jetbrains.dokka:kotlin-as-java-plugin:1.4.20"
+    val gson = "com.google.code.gson:gson:2.8.6"
+    val moshi = "com.squareup.moshi:moshi:1.11.0"
+    val moshCodeGen = "com.squareup.moshi:moshi-kotlin-codegen:1.11.0"
 }
 
 object Deps {
     val core = Core()
     val testing = Test()
+    val utils = Utils()
 }
 
