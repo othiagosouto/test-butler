d2930a07ba7e9c0d1d22db863db7aa419fc4657b
Thiago Souto <soutosss@gmail.com>
2023-08-13 16:05:45 +0200

    Add kdoc to UrlNotMockedException

--- webserver/src/main/java/dev/thiagosouto/webserver/UrlNotMockedException.kt
+++ webserver/src/main/java/dev/thiagosouto/webserver/UrlNotMockedException.kt
@@ -1,3 +1,6 @@
 package dev.thiagosouto.webserver
 
+/**
+ * Thrown to indicate that a request to a not mocked url has been made.
+ */
 class UrlNotMockedException(url: String) : Exception("Url $url is not mocked")
