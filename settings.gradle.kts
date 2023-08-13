86534d741ce920c5d587b6174f9b43ef26f82b17
Thiago Souto <soutosss@gmail.com>
2023-08-13 14:08:44 +0200

    Add plugins management to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
 include(":file-butler")
 include(":webserver")
 rootProject.name = "Test butler"
